7da849b920af6373fd8230d933b7048ac9ba3fa0
aecho <aecho1028@gmail.com>
2016-03-04 10:44:08 +0800

    Add template codes about executing remote command.

--- scp_local_template.sh
+++ scp_local_template.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-local_file=
+## scp copy file to remote server
+local_file=""
 
-dest_serv=
-dest_dir=
+dest_serv=""
+dest_dir=""
 
-scp local_file ${dest_serv}:${dest_dir}
+scp ${local_file} ${dest_serv}:${dest_dir}
+
+## Execute remote command 
+#remote_command=""
+#ssh ${dest_serv} ${remote_command}
